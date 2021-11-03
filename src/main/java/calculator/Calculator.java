57cf0da2dc9dc88c29ca77ef97af38a523946123
aiden.sim <aiden.sim@kakaocorp.com>
2021-11-03 23:28:32 +0900

    [feature] 실패하는 테스트에 대한 기능 만들기

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,23 @@
 package calculator;
 
 public class Calculator {
-    public static int calculate(String s) {
-        return 3;
+    public static int calculate(String text) {
+        String[] values = text.split(" ");
+        int first = Integer.parseInt(values[0]);
+        int second = Integer.parseInt(values[2]);
+        String operator = values[1];
+
+        int result = 0;
+        if (operator.equals("+")) {
+            result = first + second;
+        } else if (operator.equals("-")) {
+            result = first - second;
+        } else if (operator.equals("*")) {
+            result = first * second;
+        } else if (operator.equals("/")) {
+            result = first / second;
+        }
+
+        return result;
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,7 +9,7 @@ public class CalculatorTest {
     @Test
     void 덧셈() {
         int number = Calculator.calculate("1 + 1");
-        assertThat(number).isEqualTo(3);
+        assertThat(number).isEqualTo(2);
     }
 
     @Test
@@ -20,11 +20,13 @@ public class CalculatorTest {
 
     @Test
     void 곱셈() {
-
+        int number = Calculator.calculate("2 * 1");
+        assertThat(number).isEqualTo(2);
     }
 
     @Test
     void 나눗셈() {
-
+        int number = Calculator.calculate("4 / 2");
+        assertThat(number).isEqualTo(2);
     }
 }
